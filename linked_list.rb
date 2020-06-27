b9efcfddac467f5d7cb48dab717588175fec86db
Dylan Williams <happyfrog578+github@gmail.com>
2020-06-27 20:59:03 +1000

    Initial commit and framework

---
+++ linked_list.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Represents the full list
+class LinkedList
+  def append(value) end #TODO
+
+  def prepend(value) end #TODO
+
+  def size #TODO
+  end
+
+  def head #TODO
+  end
+
+  def at(index) end #TODO
+
+  def pop #TODO
+  end
+
+  def contains?(value) end #TODO
+
+  def find(value) end #TODO
+
+  def to_s #TODO
+  end
+
+  def insert_at(value, index) end # Extra credit
+
+  def remove_at(index) end # Extra credit
+
+end
---
+++ node.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Represents a single node in the list
+class Node
+  def value(value = nil) end # TODO
+
+  def next_node(value = nil) end # TODO
+end
